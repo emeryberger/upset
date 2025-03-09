743da2184dff71fc458ff10f0f0266c70238bd7b
Emery Berger <emery.berger@gmail.com>
2025-03-09 15:43:14 -0400

    Hash the whole list.

--- test/setmeup.py
+++ test/setmeup.py
@@ -1,3 +1,5 @@
+import hashlib
+import json
 class A:
     def __init__(self, v):
         self.v = v
@@ -5,6 +7,13 @@ class A:
     def __repr__(self):
         return f"{self.v}"
 
-orig_list = [A(i % 10) for i in range(1_000)]
+orig_list = [A(i % 10) for i in range(100)]
 x = list(set(orig_list))
-print(x[0])
+# print(x[0:10])
+
+# Convert entire list to SHA256 and print decimal value of first character
+
+x_json = json.dumps(x, default=lambda o: o.__dict__).encode('utf-8')
+ch = hashlib.sha256(x_json).hexdigest()[0]
+value = int(ch, base=16)
+print(value)
