df8841df0b65b90e79cd9a5bef756057ebd1e07f
Emery Berger <emery.berger@gmail.com>
2025-03-09 15:58:46 -0400

    Some cleanup.

--- src/shuffleheap.h
+++ src/shuffleheap.h
@@ -11,14 +11,7 @@
 #include "modulo.h"
 #include "randomnumbergenerator.h"
 
-#define FIXED_SIZE 0
-
 template <int NObjects,
-
-#if FIXED_SIZE
-	  size_t Size,
-#endif
-
 	  class SuperHeap>
 class ShuffleHeap : public SuperHeap {
 public:
@@ -26,35 +19,20 @@ public:
   ShuffleHeap()
     : _reqSize (0)
   {
-#if FIXED_SIZE
-    fill (Size);
-#endif
   }
 
 
   inline void * malloc (size_t sz) {
-#if FIXED_SIZE
-    assert (sz <= Size);
-    reqSize = Size;
-#else
     if (_reqSize == 0) {
-      // Get an object from the superheap and see how big it really is;
-      // fill the buffer and then use that request size going forwards.
-      void * ptr = SuperHeap::malloc(sz);
-      size_t s   = SuperHeap::getSize(ptr);
-      SuperHeap::free (ptr);
-      _reqSize = s;
-      assert (sz <= s);
-      fill (_reqSize);
+      fillBuffer(sz);
     }
-#endif
     assert (sz <= _reqSize);
     // Get an item from the superheap and swap it with a
     // randomly-chosen object from the array. This is one step of an
     // in-place Fisher-Yates shuffle.
     void * ptr = SuperHeap::malloc (_reqSize);
     int j = modulo<NObjects>(_rng.next());
-    swap (_buffer[j], ptr);
+    swap(_buffer[j], ptr);
     return ptr;
   }
 
@@ -63,33 +41,36 @@ public:
     // Choose a random item and evict it to the superheap,
     // replacing it with the one we are now freeing.
     int j = modulo<NObjects>(_rng.next());
-    swap (_buffer[j], ptr);
+    swap(_buffer[j], ptr);
     SuperHeap::free (ptr);
   }
 
  
 private:
 
+  void fillBuffer(size_t sz) {
+      // Get an object from the superheap and see how big it really is;
+      // fill the buffer and then use that request size going forwards.
+      void * ptr = SuperHeap::malloc(sz);
+      size_t s   = SuperHeap::getSize(ptr);
+      SuperHeap::free (ptr);
+      _reqSize = s;
+      assert(sz <= s);
+      fill(_reqSize);
+  }
+  
   void fill (size_t sz) {
     // Fill up the buffer from the superheap. We'll allocate randomly from this buffer.
     for (int i = 0; i < NObjects; i++) {
       _buffer[i] = SuperHeap::malloc (sz);
     }
-#if 1
-    // Now shuffle it (Fisher-Yates).
-    for (int i = NObjects - 1; i >= 1; i--) {
-      // Pick a random integer, 0 ≤ j ≤ i, and swap with item i.
-      int j = _rng.next() % (i+1);
-      swap (_buffer[i], _buffer[j]);
-    }
-#endif
   }
 
   /// Object request size
   size_t _reqSize;
 
   /// The random number generator, used for shuffling and random selection.
-  RandomNumberGenerator 	_rng;
+  RandomNumberGenerator _rng;
 
   /// The buffer used to hold shuffled objects for heap requests.
   std::array<void *, NObjects> 	_buffer;
