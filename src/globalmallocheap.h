a7b1943b18acb7cf3d64339edce2809dd09ccb61
Emery Berger <emery.berger@gmail.com>
2025-03-11 19:52:10 -0400

    Use Kingsley allocator as backing store.

--- src/globalmallocheap.h
+++ src/globalmallocheap.h
@@ -13,6 +13,16 @@ class GlobalMallocHeap : public MallocHeap {};
 
 #elif defined(__GNUC__) && !defined(__SVR4)
 
+extern "C" {
+  void * kmalloc(size_t);
+  void kfree(void *);
+  void * kcalloc(size_t, size_t);
+  void * krealloc(void *, size_t);
+  size_t kmalloc_usable_size(void *);
+}
+
+#include "kmalloc.c"
+
 class GlobalMallocHeap : public MallocHeap {
 public:
   
@@ -20,43 +30,27 @@ public:
   
   GlobalMallocHeap()
   {
-    init();
   }
 
   void *malloc(size_t size) {
-    if (!real_malloc) {
-      init();
-    }
     return real_malloc(size);
   }
   
   void free(void *ptr) {
-    if (!real_free) {
-      init();
-    }
     real_free(ptr);
   }
 
   void *calloc(size_t nmemb, size_t size) {
-    if (!real_calloc) {
-      init();
-    }
     void *ptr = real_calloc(nmemb, size);
     return ptr;
   }
 
   void *realloc(void *ptr, size_t size) {
-    if (!real_realloc) {
-      init();
-    }
     void *new_ptr = real_realloc(ptr, size);
     return new_ptr;
   }
 
   size_t getSize(void * ptr) {
-    if (!real_getsize) {
-      init();
-    }
     return real_getsize(ptr);
   }
   
@@ -68,20 +62,11 @@ private:
   typedef void *(*realloc_fn)(void *, size_t);
   typedef size_t (*getsize_fn)(void *);
   
-  malloc_fn real_malloc = (malloc_fn) nullptr;
-  free_fn real_free = (free_fn) nullptr;
-  calloc_fn real_calloc = (calloc_fn) nullptr;
-  realloc_fn real_realloc = (realloc_fn) nullptr;
-  getsize_fn real_getsize = (getsize_fn) nullptr;
-  
-  void init() {
-    real_malloc = (malloc_fn) dlsym(RTLD_NEXT, "malloc");
-    real_free = (free_fn) dlsym(RTLD_NEXT, "free");
-    real_calloc = (calloc_fn) dlsym(RTLD_NEXT, "calloc");
-    real_realloc = (realloc_fn) dlsym(RTLD_NEXT, "realloc");
-    real_getsize = (getsize_fn) dlsym(RTLD_NEXT, "malloc_usable_size");
-  }
-
+  malloc_fn real_malloc = kmalloc;
+  free_fn real_free = kfree;
+  calloc_fn real_calloc = kcalloc;
+  realloc_fn real_realloc = krealloc;
+  getsize_fn real_getsize = kmalloc_usable_size;
 };
 
 #endif
